83304e8c8af457d2af9d46600680b0ed9aff38b1
Asadullah Rahoo <asadrahu60@gmail.com>
2025-08-13 12:34:28 +0200

    reformated

--- test/test_context_engine.py
+++ test/test_context_engine.py
@@ -5,9 +5,9 @@ import pytest
 @pytest.mark.parametrize(
     "distance,lux,motion,expected",
     [
-        (40, 100, 0, "near"),      # very close
+        (40, 100, 0, "near"),  # very close
         (None, 6000, 0, "outdoor"),  # bright ambient
-        (80, 300, 0, "desk"),      # default
+        (80, 300, 0, "desk"),  # default
     ],
 )
 def test_classify(distance, lux, motion, expected):
