bb0ec246d62713838cdc653b380823e6c0ee2eee
AsadRahu60 <57444729+AsadRahu60@users.noreply.github.com>
2025-08-17 12:26:50 +0200

    Create test_lens.py
    test_lens

---
+++ test/test_lens.py
@@ -0,0 +1,5 @@
+from host.pi.lens_controller import ramp_focus
+
+def test_ramp_focus_runs():
+    ramp_focus(0, 1, 2)  # should not raise
+    assert True  # If no exception is raised, the test passes
