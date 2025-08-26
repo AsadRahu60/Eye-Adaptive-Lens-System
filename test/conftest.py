30745130ae4adc1e6700f09d9bb06342521f7c79
Asadullah Rahoo <asadrahu60@gmail.com>
2025-08-26 15:50:44 +0200

    changes_error

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,6 @@
 # test/conftest.py
-import sys, pathlib
+import sys
+import pathlib
 
 # add repo root to import path (so 'host.pi.*' works locally like on CI)
 sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1]))
