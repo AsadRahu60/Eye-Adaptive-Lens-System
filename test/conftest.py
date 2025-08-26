5fc687eb3b2b7987980590fe06b1bd48df8b4e52
Asadullah Rahoo <asadrahu60@gmail.com>
2025-08-26 15:48:02 +0200

    black

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,5 @@
 # test/conftest.py
 import sys, pathlib
+
 # add repo root to import path (so 'host.pi.*' works locally like on CI)
 sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1]))
